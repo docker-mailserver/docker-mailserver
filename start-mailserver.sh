962a98823e3b69a14c242eaac5fa3778c223e02f
Thomas VIAL <github@ifusio.com>
2015-05-02 19:05:34 +0200

    Added virtual aliases management using dedicated env variable.

--- start-mailserver.sh
+++ start-mailserver.sh
@@ -2,29 +2,41 @@
 
 echo "Regenerating 'vmailbox' for given users"
 
-echo "docker_mail_users => $docker_mail_users"
-
 echo "# WARNING: this file is auto-generated. Do not modify locally" > /etc/postfix/vmailbox
 echo $docker_mail_users | sed -r 's/\[|\]|\x27| //g' | sed -r 's/,/\n/g' > /tmp/docker_mail_users
 while IFS=$'|' read -r login pass
 do
-	# Setting variables for better readability
-	user=$(echo ${login} | cut -d @ -f1)
-	domain=$(echo ${login} | cut -d @ -f2)
+  # Setting variables for better readability
+  user=$(echo ${login} | cut -d @ -f1)
+  domain=$(echo ${login} | cut -d @ -f2)
 
-	# Let's go!
-	echo "user '${user}' for domain '${domain}' with password '${pass}'"
-	echo "${login} ${domain}/${user}/" >> /etc/postfix/vmailbox
-	userdb ${login} set uid=5000 gid=5000 home=/var/mail/${domain}/${user} mail=/var/mail/${domain}/${user}
-	echo "${pass}" | userdbpw -md5 | userdb ${login} set systempw
-	echo "${pass}" | saslpasswd2 -p -c -u ${domain} ${login}
-	mkdir -p /var/mail/${domain}
-	maildirmake /var/mail/${domain}/${user}
-	echo ${domain} >> /tmp/vhost.tmp
+  # Let's go!
+  echo "user '${user}' for domain '${domain}' with password '${pass}'"
+  echo "${login} ${domain}/${user}/" >> /etc/postfix/vmailbox
+  userdb ${login} set uid=5000 gid=5000 home=/var/mail/${domain}/${user} mail=/var/mail/${domain}/${user}
+  echo "${pass}" | userdbpw -md5 | userdb ${login} set systempw
+  echo "${pass}" | saslpasswd2 -p -c -u ${domain} ${login}
+  mkdir -p /var/mail/${domain}
+  maildirmake /var/mail/${domain}/${user}
+  echo ${domain} >> /tmp/vhost.tmp
 
 done < /tmp/docker_mail_users
 rm /tmp/docker_mail_users
 makeuserdb
+
+echo "Regenerating 'virtual' for given aliases"
+echo $docker_mail_aliases | sed -r 's/\[|\]|\x27|//g' | sed -r 's/, /\n/g' > /tmp/docker_mail_aliases
+while IFS=$'|' read -r login aliases
+do
+  arr=$(echo $aliases | tr "," "\n")
+  for alias in $arr
+  do
+    user=$(echo ${login} | cut -d @ -f1)
+    domain=$(echo ${login} | cut -d @ -f2)
+    echo "$alias@$domain redirects to $login"
+    echo "$alias@$domain\t$login" >> /etc/postfix/virtual
+  done
+done < /tmp/docker_mail_aliases
 
 echo "Postfix configurations"
 postmap /etc/postfix/vmailbox
