74452917fb90fd4a257ebb701f20b3ad6e713f97
Thomas VIAL <github@ifusio.com>
2015-10-19 12:06:11 +0200

    Added tests on error logs

--- test/test.sh
+++ test/test.sh
@@ -12,7 +12,7 @@ assert_raises "docker exec mail ps aux --forest | grep '/usr/sbin/amavisd-new'" 
 
 # Testing user creation
 assert "docker exec mail ls -A /var/mail/localhost.localdomain/user1" "cur\nnew\ntmp"
-assert "docker exec mail ls /var/mail/otherdomain.tld/user2" "cur\nnew\ntmp"
+assert "docker exec mail ls -A /var/mail/otherdomain.tld/user2" "cur\nnew\ntmp"
 
 # Testing that mail is received for existing user
 assert_raises "docker exec mail grep 'status=sent (delivered to maildir)' /var/log/mail.log" "false"
@@ -20,6 +20,10 @@ assert "docker exec mail ls -A /var/mail/localhost.localdomain/user1/new | wc -l
 
 # Testing presence of freshclam CRON
 assert "docker exec mail crontab -l" "0 1 * * * /usr/bin/freshclam --quiet"
+
+# Testing that log don't display errors
+assert_raises "docker exec mail grep 'non-null host address bits in' /var/log/mail.log" "false"
+assert_raises "docker exec mail grep ': error:' /var/log/mail.log" "false"
 
 # Ending tests
 assert_end 
