a6fbef98355da91530768cc63fa24c907b528a94
Jonas Kalderstam <jonas@kalderstam.se>
2015-12-08 01:59:45 +0100

    Do not create dir if it exists

--- start-mailserver.sh
+++ start-mailserver.sh
@@ -25,7 +25,12 @@ if [ -f /tmp/postfix/accounts.cf ]; then
     echo "${pass}" | userdbpw -md5 | userdb ${login} set systempw
     echo "${pass}" | saslpasswd2 -p -c -u ${domain} ${login}
     mkdir -p /var/mail/${domain}
-    maildirmake /var/mail/${domain}/${user}
+    if [ -d "/var/mail/${domain}/${user}" ]; then
+      # Directory already present
+      echo "Directory present"
+    else
+      maildirmake "/var/mail/${domain}/${user}"
+    fi
     echo ${domain} >> /tmp/vhost.tmp
   done < /tmp/postfix/accounts.cf
   makeuserdb
