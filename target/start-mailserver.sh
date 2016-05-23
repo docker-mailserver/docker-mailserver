7f398a6bbcab09b2c8024f9e5d2f77c92db4bc3f
Thorsten von Eicken <tve@rightscale.com>
2016-05-22 23:35:09 -0700

    fix sed of main.cf for regexp aliases, try #3

--- target/start-mailserver.sh
+++ target/start-mailserver.sh
@@ -82,7 +82,10 @@ if [ -f /tmp/docker-mailserver/postfix-regexp.cf ]; then
   # Copying regexp alias file
   echo "Adding regexp alias file postfix-regexp.cf"
   cp /tmp/docker-mailserver/postfix-regexp.cf /etc/postfix/regexp
-  sed -i -e "/^virtual_alias_maps/a| regexp:/etc/postfix/regexp|" /etc/postfix/main.cf
+  sed -i -e '/^virtual_alias_maps/{
+    s/ regexp:.*//
+    s/$/ regexp:\/etc\/postfix\/regexp/
+    }' /etc/postfix/main.cf
 fi
 
 # DKIM
