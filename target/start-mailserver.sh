e448235affb8e45308e39ec57f76480e83ace2eb
alinmear <alinmear@users.noreply.github.com>
2017-07-01 21:32:03 +0200

    Fix typo

--- target/start-mailserver.sh
+++ target/start-mailserver.sh
@@ -353,7 +353,7 @@ function override_config() {
 	    do
 		[ -z $key ] && echo -e "\t no key provided" && return 1
 
-		sed -i -e "s|^${key}[[:space:]]\+.*|g${key} = ${config_overrides[$key]//&/\\&}|g" \
+		sed -i -e "s|^${key}[[:space:]]\+.*|${key} = ${config_overrides[$key]//&/\\&}|g' \
 		    ${f}
 	    done
 	fi
@@ -913,16 +913,6 @@ function _setup_postfix_override_configuration() {
 	else
 		notify 'inf' "No extra postfix settings loaded because optional '/tmp/docker-mailserver/postfix-main.cf' not provided."
 	fi
-	if [ -f /tmp/docker-mailserver/postfix-master.cf ]; then
-		while read line; do
-		if [[ "$line" =~ ^[a-z] ]]; then
-			postconf -P "$line"
-		fi
-		done < /tmp/docker-mailserver/postfix-master.cf
-		notify 'inf' "Loaded 'config/postfix-master.cf'"
-	else
-		notify 'inf' "No extra postfix settings loaded because optional '/tmp/docker-mailserver/postfix-master.cf' not provided."
-	fi
 }
 
 function _setup_postfix_sasl_password() {
@@ -979,6 +969,7 @@ function _setup_security_stack() {
 		SA_TAG=${SA_TAG:="2.0"} && sed -i -r 's/^\$sa_tag_level_deflt (.*);/\$sa_tag_level_deflt = '$SA_TAG';/g' /etc/amavis/conf.d/20-debian_defaults
 		SA_TAG2=${SA_TAG2:="6.31"} && sed -i -r 's/^\$sa_tag2_level_deflt (.*);/\$sa_tag2_level_deflt = '$SA_TAG2';/g' /etc/amavis/conf.d/20-debian_defaults
 		SA_KILL=${SA_KILL:="6.31"} && sed -i -r 's/^\$sa_kill_level_deflt (.*);/\$sa_kill_level_deflt = '$SA_KILL';/g' /etc/amavis/conf.d/20-debian_defaults
+		SA_SPAM_SUBJECT=${SA_SPAM_SUBJECT:="***SPAM*** "} && sed -i -r 's/^\$sa_spam_subject_tag (.*);/\$sa_spam_subject_tag = '"'$SA_SPAM_SUBJECT'"';/g' /etc/amavis/conf.d/20-debian_defaults
 		test -e /tmp/docker-mailserver/spamassassin-rules.cf && cp /tmp/docker-mailserver/spamassassin-rules.cf /etc/spamassassin/
 	fi
 
