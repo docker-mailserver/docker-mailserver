37708b57876e665c615602b2715e2e1d0acff0af
Erik Wramner <erik@wramner.name>
2019-07-31 10:41:32 +0200

    Added comment explaining chksum file location

--- target/check-for-changes.sh
+++ target/check-for-changes.sh
@@ -7,6 +7,8 @@ sleep 5
 echo "${log_date} Start check-for-changes script."
 
 # create work area outside mounted directory
+# the checksum file should be reused on subsequent runs,
+# but only by this container, not by others using same mount
 mkdir -p /tmp/docker-mailserver-work
 CHKSUM_FILE=/tmp/docker-mailserver-work/chksum
 
