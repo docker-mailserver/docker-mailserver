3513944d0a66b13ab4a3779cf4caea4ef935e53f
Martin Schulze <martin.schulze@vireso.de>
2019-09-11 01:01:51 +0200

    Skip all tests of file after failed setup_file

--- test/test_helper/common.bash
+++ test/test_helper/common.bash
@@ -34,16 +34,26 @@ function wait_for_finished_setup_in_container() {
     repeat_until_success_or_timeout $TEST_TIMEOUT_IN_SECONDS sh -c "docker logs $1 | grep 'Starting mail server'"
 }
 
+SETUP_FILE_MARKER="$BATS_TMPDIR/`basename \"$BATS_TEST_FILENAME\".setup_file`"
+
 # use in setup() in conjunction with a `@test "first" {}` to trigger setup_file reliably
 function run_setup_file_if_necessary() {
     if [ "$BATS_TEST_NAME" == 'test_first' ]; then
+        rm -f "$SETUP_FILE_MARKE"
         setup_file
+        touch "$SETUP_FILE_MARKER"
+    else
+        if [ ! -f "$SETUP_FILE_MARKER" ]; then
+            skip "setup_file failed"
+            return 1
+        fi
     fi
 }
 
 # use in teardown() in conjunction with a `@test "last" {}` to trigger teardown_file reliably
 function run_teardown_file_if_necessary() {
     if [ "$BATS_TEST_NAME" == 'test_last' ]; then
+        rm -f "$SETUP_FILE_MARKE"
         teardown_file
     fi
 }
