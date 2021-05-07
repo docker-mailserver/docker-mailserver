57bb66be884adb574912a5ade6ba8a92612c6652
casperklein <casperklein@users.noreply.github.com>
2021-05-07 20:11:37 +0200

    make script ugly, to satisfy linter..

--- target/scripts/update-check.sh
+++ target/scripts/update-check.sh
@@ -19,16 +19,16 @@ do
     if dpkg --compare-versions "${VERSION}" lt "${LATEST}"
     then
       # send mail notification to postmaster
-      read -r -d '' MAIL <<- EOM
-	Hello ${POSTMASTER_ADDRESS}!
+      read -r -d '' MAIL << EOM
+Hello ${POSTMASTER_ADDRESS}!
 
-	There is a docker-mailserver update available on your host: $(hostname -f)
+There is a docker-mailserver update available on your host: $(hostname -f)
 
-	Current version: ${VERSION}
-	Latest  version: ${LATEST}
+Current version: ${VERSION}
+Latest  version: ${LATEST}
 
-	Changelog: https://github.com/docker-mailserver/docker-mailserver/blob/master/CHANGELOG.md
-	EOM
+Changelog: https://github.com/docker-mailserver/docker-mailserver/blob/master/CHANGELOG.md
+EOM
       echo "${MAIL}" | mail -s "Update available! [ ${VERSION} --> ${LATEST} ]" "${POSTMASTER_ADDRESS}" && \
 
       echo "${DATE} Info: Update available [ ${VERSION} --> ${LATEST} ]" && \
