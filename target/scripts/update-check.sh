d748b07a15175cc6fa341edc38ec82c27977ace5
casperklein <casperklein@users.noreply.github.com>
2021-05-17 18:07:59 +0200

    VERSION --> DMS_VERSION

--- target/scripts/update-check.sh
+++ target/scripts/update-check.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-VERSION=$(</VERSION)
-
 while true
 do
   DATE=$(date '+%F %T')
@@ -16,7 +14,7 @@ do
     echo "${DATE} Info: Remote version information fetched"
 
     # compare versions
-    if dpkg --compare-versions "${VERSION}" lt "${LATEST}"
+    if dpkg --compare-versions "${DMS_VERSION}" lt "${LATEST}"
     then
       # send mail notification to postmaster
       read -r -d '' MAIL << EOM
@@ -24,14 +22,14 @@ Hello ${POSTMASTER_ADDRESS}!
 
 There is a docker-mailserver update available on your host: $(hostname -f)
 
-Current version: ${VERSION}
+Current version: ${DMS_VERSION}
 Latest  version: ${LATEST}
 
 Changelog: https://github.com/docker-mailserver/docker-mailserver/blob/master/CHANGELOG.md
 EOM
-      echo "${MAIL}" | mail -s "Update available! [ ${VERSION} --> ${LATEST} ]" "${POSTMASTER_ADDRESS}" && \
+      echo "${MAIL}" | mail -s "Update available! [ ${DMS_VERSION} --> ${LATEST} ]" "${POSTMASTER_ADDRESS}" && \
 
-      echo "${DATE} Info: Update available [ ${VERSION} --> ${LATEST} ]" && \
+      echo "${DATE} Info: Update available [ ${DMS_VERSION} --> ${LATEST} ]" && \
 
       # only notify once
       exit 0
