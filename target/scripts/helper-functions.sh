c4a8a8d5c0e54f43b661ce1f5899f70d339f0932
NorseGaud <connarpierce@gmail.com>
2021-09-11 22:31:27 -0400

    added uuid to Dockerfile

--- target/scripts/helper-functions.sh
+++ target/scripts/helper-functions.sh
@@ -19,22 +19,22 @@ function escape
 
 function create_lock
 {
-    LOCK_FILE="/tmp/docker-mailserver/${SCRIPT_NAME}.lock"
-    while [[ -e "${LOCK_FILE}" ]]
-    do
-      _notify 'warn' "Lock file ${LOCK_FILE} exists. Another ${SCRIPT_NAME} execution is happening. Trying again shortly..."
-      # Handle stale lock files left behind on crashes
-      # or premature/non-graceful exits of containers while they're making changes
-      if [[ -n "$(find "${LOCK_FILE}" -mmin +1 2>/dev/null)" ]]
-      then
-        _notify 'warn' "Lock file older than 1 minute. Removing stale lock file."
-        rm -f "${LOCK_FILE}"
-        _notify 'inf' "Removed stale lock ${LOCK_FILE}."
-      fi
-      sleep 5
-    done
-    trap remove_lock EXIT
-    echo "${LOCK_ID}" > "${LOCK_FILE}"
+  LOCK_FILE="/tmp/docker-mailserver/${SCRIPT_NAME}.lock"
+  while [[ -e "${LOCK_FILE}" ]]
+  do
+    _notify 'warn' "Lock file ${LOCK_FILE} exists. Another ${SCRIPT_NAME} execution is happening. Trying again shortly..."
+    # Handle stale lock files left behind on crashes
+    # or premature/non-graceful exits of containers while they're making changes
+    if [[ -n "$(find "${LOCK_FILE}" -mmin +1 2>/dev/null)" ]]
+    then
+      _notify 'warn' "Lock file older than 1 minute. Removing stale lock file."
+      rm -f "${LOCK_FILE}"
+      _notify 'inf' "Removed stale lock ${LOCK_FILE}."
+    fi
+    sleep 5
+  done
+  trap remove_lock EXIT
+  echo "${LOCK_ID}" > "${LOCK_FILE}"
 }
 
 function remove_lock
