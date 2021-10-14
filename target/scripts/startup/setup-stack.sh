36ef74857e068eac4b4a5bdd2df3d54bf2c2bdd8
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2021-10-14 20:00:05 +0200

    add aliases to Dovecots userdb & adjust the passdb/userdb configuration

--- target/scripts/startup/setup-stack.sh
+++ target/scripts/startup/setup-stack.sh
@@ -330,6 +330,7 @@ function _setup_dovecot_local_user
 
     # creating users ; 'pass' is encrypted
     # comments and empty lines are ignored
+    local LOGIN PASS USER_ATTRIBUTES
     while IFS=$'|' read -r LOGIN PASS USER_ATTRIBUTES
     do
       # Setting variables for better readability
@@ -346,18 +347,23 @@ function _setup_dovecot_local_user
         [[ ${#USER_QUOTA[@]} -eq 2 ]] && USER_ATTRIBUTES="${USER_ATTRIBUTES} userdb_quota_rule=*:bytes=${USER_QUOTA[1]}"
       fi
 
-      # Let's go!
-      _notify 'inf' "user '${USER}' for domain '${DOMAIN}' with password '********', attr=${USER_ATTRIBUTES}"
+      if [[ -z ${USER_ATTRIBUTES} ]]
+      then
+        _notify 'inf' "Creating user '${USER}' for domain '${DOMAIN}'"
+      else
+        _notify 'inf' "Creating user '${USER}' for domain '${DOMAIN}' with attributes '${USER_ATTRIBUTES}'"
+      fi
 
       echo "${LOGIN} ${DOMAIN}/${USER}/" >> /etc/postfix/vmailbox
-      # User database for dovecot has the following format:
-      # user:password:uid:gid:(gecos):home:(shell):extra_fields
-      # Example :
-      # ${LOGIN}:${PASS}:5000:5000::/var/mail/${DOMAIN}/${USER}::userdb_mail=maildir:/var/mail/${DOMAIN}/${USER}
-      echo "${LOGIN}:${PASS}:5000:5000::/var/mail/${DOMAIN}/${USER}::${USER_ATTRIBUTES}" >> /etc/dovecot/userdb
+      # Dovecot's userdb has the following format
+      #   user:password:uid:gid:(gecos):home:(shell):extra_fields
+      echo \
+        "${LOGIN}:${PASS}:5000:5000::/var/mail/${DOMAIN}/${USER}::${USER_ATTRIBUTES}" \
+        >>/etc/dovecot/userdb
+
       mkdir -p "/var/mail/${DOMAIN}/${USER}"
 
-      # Copy user provided sieve file, if present
+      # copy user provided sieve file, if present
       if [[ -e "/tmp/docker-mailserver/${LOGIN}.dovecot.sieve" ]]
       then
         cp "/tmp/docker-mailserver/${LOGIN}.dovecot.sieve" "/var/mail/${DOMAIN}/${USER}/.dovecot.sieve"
@@ -365,6 +371,48 @@ function _setup_dovecot_local_user
 
       echo "${DOMAIN}" >> /tmp/vhost.tmp
     done < <(grep -v "^\s*$\|^\s*\#" /tmp/docker-mailserver/postfix-accounts.cf)
+
+    # adding aliases to Dovecot's userdb
+    local ALIAS REAL_USER
+    while read -r ALIAS REAL_USER
+    do
+      [[ ${ALIAS} == \#* ]] && continue
+
+      # setting variables for better readability
+      USER=$(echo "${REAL_USER}" | cut -d @ -f1)
+      DOMAIN=$(echo "${REAL_USER}" | cut -d @ -f2)
+
+      _notify 'inf' "Adding alias '${ALIAS}' for user '${REAL_USER}' to Dovecot userdb"
+
+      local PASS USER_ATTRIBUTES
+      while IFS=$'|' read -r LOGIN REAL_PASS USER_ATTRIBUTES
+      do
+        PASS=${REAL_PASS}
+        USER_ATTRIBUTES
+        break
+      done < <(grep "${REAL_USER}" /tmp/docker-mailserver/postfix-accounts.cf)
+
+      # test if user has a defined quota
+      if [[ -f /tmp/docker-mailserver/dovecot-quotas.cf ]]
+      then
+        declare -a USER_QUOTA
+        IFS=':' ; read -r -a USER_QUOTA < <(grep "${USER}@${DOMAIN}:" -i /tmp/docker-mailserver/dovecot-quotas.cf)
+        unset IFS
+
+        [[ ${#USER_QUOTA[@]} -eq 2 ]] && USER_ATTRIBUTES="${USER_ATTRIBUTES} userdb_quota_rule=*:bytes=${USER_QUOTA[1]}"
+      fi
+
+      if [[ -z ${PASS} ]]
+      then
+        _notify 'war' "Could not acquire password of user '${REAL_USER}' while adding alias '${ALIAS}'"
+        continue
+      fi
+
+      echo \
+        "${ALIAS}:${PASS}:5000:5000::/var/mail/${DOMAIN}/${USER}::${USER_ATTRIBUTES:-}" \
+        >> /etc/dovecot/userdb
+
+    done < <(cat /tmp/docker-mailserver/postfix-virtual.cf)
   else
     _notify 'inf' "'/tmp/docker-mailserver/postfix-accounts.cf' is not provided. No mail account created."
   fi
