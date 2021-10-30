853164d8ddc7ae6527ddc81f5c443b65b178fe01
polarathene <5098581+polarathene@users.noreply.github.com>
2021-10-30 20:26:51 +1300

    ci(docs): Added inline docs
    Extra documentation context for maintainers to quickly grok what's going on.

--- .github/workflows/scripts/docs/build-docs.sh
+++ .github/workflows/scripts/docs/build-docs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 
+# PWD should be at the project docs/ folder.
 # `--user` is required for build output file ownership to match the CI user,
 #  instead of the internal root user of the container.
+# `build --strict` ensures the build fails when any warnings are omitted.
 docker run \
   --rm \
   --user "$(id -u):$(id -g)" \
@@ -11,6 +13,7 @@ docker run \
   squidfunk/mkdocs-material:7.3.5 build --strict
 
 # Remove unnecessary build artifacts: https://github.com/squidfunk/mkdocs-material/issues/2519
+# site/ is the build output folder.
 cd site
 find . -type f -name '*.min.js.map' -delete -o -name '*.min.css.map' -delete
 rm sitemap.xml.gz
