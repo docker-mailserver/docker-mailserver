4b90e6d876e8478214ca2f6b9baf7e318dc92991
polarathene <5098581+polarathene@users.noreply.github.com>
2021-10-30 20:06:23 +1300

    fix: Fail when build aborts from broken links
    Using `set -ex` will exit the script as soon as a non-zero exit code is returned, such as when the docker image fails building the docs due to `build --strict` catching broken links.
    
    This removes the need for `|| exit`. `-x` is used to improve command visibility in the workflow log output.
    
    `--name` isn't necessary, but was part of the original PR. I've chosen not to include `-o pipefail` only because no pipes are used in the present script.

--- .github/workflows/scripts/docs/build-docs.sh
+++ .github/workflows/scripts/docs/build-docs.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+set -ex
 
 # `--user` is required for build output file ownership to match the CI user,
 #  instead of the internal root user of the container.
 docker run \
   --rm \
   --user "$(id -u):$(id -g)" \
-  -v "${PWD}:/docs" \
+  --volume "${PWD}:/docs" \
+  --name "build-docs" \
   squidfunk/mkdocs-material:7.3.5 build --strict
 
 # Remove unnecessary build artifacts: https://github.com/squidfunk/mkdocs-material/issues/2519
-cd site || exit
+cd site
 find . -type f -name '*.min.js.map' -delete -o -name '*.min.css.map' -delete
 rm sitemap.xml.gz
 rm assets/images/favicon.png
