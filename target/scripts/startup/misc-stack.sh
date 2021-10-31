79617dcc40b6fe415020b7811d58b8b33da3f00c
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2021-11-01 10:23:00 +1300

    fix: `ONE_DIR=1` should keep correct permissions for `spool-postfix` (#2273)
    GID of postfix(102) should not be set here. only GID is root(0) and postdrop(103). Ensure root(0) UID matches source location ownership too.

--- target/scripts/startup/misc-stack.sh
+++ target/scripts/startup/misc-stack.sh
@@ -59,6 +59,21 @@ function _misc_save_states
     chown -R postfix /var/mail-state/lib-postfix
     chown -R postgrey /var/mail-state/lib-postgrey
     chown -R debian-spamd /var/mail-state/lib-spamassassin
-    chown -R postfix /var/mail-state/spool-postfix
+
+    # UID = postfix(101): active, bounce, corrupt, defer, deferred, flush, hold, incoming, maildrop, private, public, saved, trace
+    # UID = root(0): dev, etc, lib, pid, usr
+    # GID = postdrop(103): maildrop, public
+    # GID for all other directories is root(0)
+    # Set most common ownership:
+    chown -R postfix:root /var/mail-state/spool-postfix
+    # These two require the postdrop(103) group:
+    chgrp -R postdrop /var/mail-state/spool-postfix/maildrop
+    chgrp -R postdrop /var/mail-state/spool-postfix/public
+    # These all have root ownership at the src location:
+    chown -R root /var/mail-state/spool-postfix/dev
+    chown -R root /var/mail-state/spool-postfix/etc
+    chown -R root /var/mail-state/spool-postfix/lib
+    chown -R root /var/mail-state/spool-postfix/pid
+    chown -R root /var/mail-state/spool-postfix/usr
   fi
 }
