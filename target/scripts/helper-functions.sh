783aab784d13d95a35bc9469577d842e9149aaf4
polarathene <5098581+polarathene@users.noreply.github.com>
2021-11-01 17:34:33 +1300

    chore: Add more comment docs for maintainers
    Despite the verbosity, it's better to make this visible here for maintenance and debugging purposes than trying to dig through issue/PR or commit history for it.

--- target/scripts/helper-functions.sh
+++ target/scripts/helper-functions.sh
@@ -302,6 +302,13 @@ function _obtain_hostname_and_domainname
 {
   # Normally this value would match the output of `hostname` which mirrors `/proc/sys/kernel/hostname`,
   # However for legacy reasons, the system ENV `HOSTNAME` was replaced here with `hostname -f` instead.
+  #
+  # TODO: Consider changing to `DMS_FQDN`; a more accurate name, and removing the `export`, assuming no
+  # subprocess like postconf would be called that would need access to the same value via `$HOSTNAME` ENV.
+  #
+  # TODO: `OVERRIDE_HOSTNAME` was introduced for non-Docker runtimes that could not configure an explicit hostname.
+  # k8s was the particular runtime in 2017. This does not update `/etc/hosts` or other locations, thus risking
+  # inconsistency with expected behaviour. Investigate if it's safe to remove support. (--net=host also uses this as a workaround)
   export HOSTNAME="${OVERRIDE_HOSTNAME:-"$(hostname -f)"}"
 
   # If the container is misconfigured.. `hostname -f` (which derives it's return value from `/etc/hosts` or DNS query),
@@ -313,14 +320,26 @@ function _obtain_hostname_and_domainname
 
   # If the `HOSTNAME` is more than 2 labels long (eg: mail.example.com),
   # We take the FQDN from it, minus the 1st label (aka _short hostname_, `hostname -s`).
+  #
+  # TODO: For some reason we're explicitly separating out a domain name from our FQDN,
+  # `hostname -d` was probably not the correct command for this intention either.
+  # Needs further investigation for relevance, and if `/etc/hosts` is important for consumers
+  # of this variable or if a more deterministic approach with `cut` should be relied on.
   if [[ $(_get_label_length "${HOSTNAME}") -gt 2 ]]
   then
     if [[ -n ${OVERRIDE_HOSTNAME} ]]
     then
-      # Emulates the intended behaviour of `hostname -d`
+      # Emulates the intended behaviour of `hostname -d`:
       DOMAINNAME="$(echo "${HOSTNAME}" | cut -d '.' -f2-99)"
     else
-      # Operates on the FQDN returned from querying `/etc/hosts` or fallback DNS
+      # Operates on the FQDN returned from querying `/etc/hosts` or fallback DNS:
+      #
+      # Note if you want the actual NIS `domainname`, use the `domainname` command,
+      # or `cat /proc/sys/kernel/domainname`.
+      # Our usage of `domainname` is under consideration as legacy, and not advised
+      # going forward. In future our docs should drop any mention of it.
+
+      #shellcheck disable=SC2034
       DOMAINNAME="$(hostname -d)"
     fi
   fi
