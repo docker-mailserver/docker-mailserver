3b66022b286cd1d3184be54485c9f577e2014b6d
polarathene <5098581+polarathene@users.noreply.github.com>
2022-06-06 11:32:53 +1200

    chore: Remove `_is_comment` helper method
    No longer serving a purpose with more appropriate filter method for pre-processing the entire config file.

--- target/scripts/helpers/utils.sh
+++ target/scripts/helpers/utils.sh
@@ -12,14 +12,6 @@ function _get_valid_lines_from_file
   grep --extended-regexp --invert-match "^\s*$|^\s*#" "${1}" || true
 }
 
-# TODO: Only used by `relay.sh`, will be removed in future.
-# Similar to _get_valid_lines_from_file, but only returns a status code
-# to indicate invalid line(s):
-function _is_comment
-{
-  grep -q -E "^\s*$|^\s*#" <<< "${1}"
-}
-
 # Provide the name of an environment variable to this function
 # and it will return its value stored in /etc/dms-settings
 function _get_dms_env_value
