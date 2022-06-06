04243d486acaca8063851ca3711e51b165dff538
polarathene <5098581+polarathene@users.noreply.github.com>
2022-06-06 16:20:36 +1200

    chore: Remove requirement for `postfix-accounts.cf`
    This is an old requirement from when the change detector service was first introduced. It's no longer relevant.

--- target/scripts/check-for-changes.sh
+++ target/scripts/check-for-changes.sh
@@ -27,12 +27,6 @@ then
   _exit_with_error "Could not change into '/tmp/docker-mailserver/' directory" 0
 fi
 
-# check postfix-accounts.cf exist else break
-if [[ ! -f postfix-accounts.cf ]]
-then
-  _exit_with_error "'/tmp/docker-mailserver/postfix-accounts.cf' is missing" 0
-fi
-
 # verify checksum file exists; must be prepared by start-mailserver.sh
 if [[ ! -f ${CHKSUM_FILE} ]]
 then
