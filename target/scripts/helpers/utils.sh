fea7d56bfcb26566815c2593b2316737403377cf
polarathene <5098581+polarathene@users.noreply.github.com>
2022-06-07 14:26:46 +1200

    chore: Extract out `/var/mail` ownership workaround
    Keep it in sync between the two locations via shared helper method.

--- target/scripts/helpers/utils.sh
+++ target/scripts/helpers/utils.sh
@@ -20,3 +20,19 @@ function _get_dms_env_value
   VALUE=$(grep "^${1}=" /etc/dms-settings | cut -d '=' -f 2)
   printf '%s' "${VALUE:1:-1}"
 }
+
+# TODO: `chown -R 5000:5000 /var/mail` has existed since the projects first commit.
+# It later received a depth guard to apply the fix only when it's relevant for a dir.
+# Assess if this still appropriate, it appears to be problematic for some LDAP users.
+#
+# `helpers/accounts.sh:_create_accounts` (mkdir, cp) appears to be the only writer to
+# /var/mail folders (used during startup and change detection handling).
+function _chown_var_mail_if_necessary
+{
+  # fix permissions, but skip this if 3 levels deep the user id is already set
+  if find /var/mail -maxdepth 3 -a \( \! -user 5000 -o \! -group 5000 \) | read -r
+  then
+    _log 'trace' 'Fixing /var/mail permissions'
+    chown -R 5000:5000 /var/mail || return 1
+  fi
+}
