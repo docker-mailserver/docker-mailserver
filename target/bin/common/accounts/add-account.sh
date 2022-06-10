1b4fc3e532c1ff742b45a50e72af2df065f0f5b4
polarathene <5098581+polarathene@users.noreply.github.com>
2022-06-10 16:31:15 +1200

    chore: Sync common code `addmailuser` + `adddovecotmasteruser`
    Majority of shared logic extracted into `add-account.sh` for maintenance.

---
+++ target/bin/common/accounts/add-account.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+function _if_missing_request_password
+{
+  if [[ -z ${PASSWD} ]]
+  then
+    read -r -s -p 'Enter Password: ' PASSWD
+    echo
+    [[ -z ${PASSWD} ]] && _exit_with_error 'Password must not be empty'
+  fi
+}
+
+function _account_already_exists
+{
+  # Escaped values for use in regex patterns:
+  local _MAIL_ACCOUNT_=$(_escape "${MAIL_ACCOUNT}")
+
+  # `|` is a delimter between the account identity (_MAIL_ACCOUNT_) and the hashed password
+  grep -qi "^${_MAIL_ACCOUNT_}|" "${DATABASE}" 2>/dev/null
+}
+
+function _add_account_to_db
+{
+  local DATABASE=${1}
+  touch "${DATABASE}"
+  _create_lock # Protect config file with lock to avoid race conditions
+
+  if _account_already_exists
+  then
+    _exit_with_error "User '${MAIL_ACCOUNT}' already exists"
+  fi
+
+  _if_missing_request_password
+  # Create the hashed password, then add an account entry into the DATABASE:
+  local HASH=$(doveadm pw -s SHA512-CRYPT -u "${MAIL_ACCOUNT}" -p "${PASSWD}")
+  echo "${MAIL_ACCOUNT}|${HASH}" >> "${DATABASE}"
+}
+
+# Tests fail if the creation of /var/mail/${DOMAIN}/${USER} doesn't happen fast enough after addmailuser executes (check-for-changes.sh race-condition)
+# Prevent infinite loop in tests like "checking accounts: user3 should have been added to /tmp/docker-mailserver/postfix-accounts.cf even when that file does not exist"
+function _wait_until_account_maildir_exists
+{
+  if [[ -e ${CHKSUM_FILE} ]]
+  then
+    local USER="${MAIL_ACCOUNT%@*}"
+    local DOMAIN="${MAIL_ACCOUNT#*@}"
+
+    while [[ ! -d "/var/mail/${DOMAIN}/${USER}" ]]
+    do
+      _log 'info' "Waiting for dovecot to create '/var/mail/${DOMAIN}/${USER}/'"
+      sleep 1
+    done
+  fi
+}
