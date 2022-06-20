151795002a91f41cb320c3b7e06c13f21e6f6218
polarathene <5098581+polarathene@users.noreply.github.com>
2022-06-20 12:45:57 +1200

    refactor: Unify virtual alias management
    Similar to previous commit for quota, the two methods with three consumers (addalias, delalias, delmailuser) adopt the same switch case approach.
    
    Validation of args inlined. Args have the same order again, no longer supporting blank `MAIL_ALIAS`, instead expecting `_` to explicitly set the scope to all aliases.
    
    `listmailuser` specific method returned to the command.

--- target/bin/common/virtual-aliases.sh
+++ target/bin/common/virtual-aliases.sh
@@ -1,65 +1,47 @@
 #! /bin/bash
 
-# Used from /usr/local/bin/helpers/index.sh:
-# _escape, _exit_with_error
+# Manage DB writes for: DATABASE_VIRTUAL
 
-# Some of these helpers rely on:
-# - Exteral vars to be declared prior to calling them (MAIL_ALIAS, RECIPIENT).
-# - Calling external method '__usage' as part of error handling.
+# A virtual alias may be any of `user@domain`, `user`, `@domain`.
+# Recipients are local (internal services), hosted (managed accounts), remote (third-party MTA), or aliases themselves,
+# An alias may redirect mail to one or more recipients. If a recipient is an alias Postfix will recursively resolve it.
+#
+# WARNING: Support for multiple and recursive recipients may not be well supported by this projects scripts/features.
+# One of those features is Dovecot Quota support, which uses a naive workaround for supporting quota checks for inbound
+# mail to an alias address.
 
-# Collection of methods to operate on virtual alias database: `postfix-virtual.cf`.
-
-# An alias may be any of `user@domain`, `user`, `@domain`.
-# Recipients are local (internal services), hosted (managed accounts), remote (third-party MTA), or aliases,
-# An alias may redirect mail to one or more recipients. If a recipient is an alias it will recursively resolve.
-# NOTE: Support for recipients of the alias type may not be well supported by this project.
-
-# Associate an alias to a recipient:
-function _alias_add_for_recipient
+# Logic to perform for requested operations handled here:
+function _manage_virtual_aliases
 {
-  local MAIL_ALIAS=${1}
-  local RECIPIENT=${2}
-
-  local DATABASE_VIRTUAL='/tmp/docker-mailserver/postfix-virtual.cf'
-  _db_entry_add_or_append "${DATABASE_VIRTUAL}" "${MAIL_ALIAS}" "${RECIPIENT}" \
-    || _exit_with_error "'${MAIL_ALIAS}' is already an alias for recipient: '${RECIPIENT}'"
-}
-
-# Used by delalias + delmailuser
-# Removes RECIPIENT from all aliases unless provided a target with MAIL_ALIAS:
-# NOTE: If a matched alias has no additional recipients, it is also removed.
-function _alias_remove_for_recipient
-{
-  local RECIPIENT=${1}
+  local ACTION=${1}
   local MAIL_ALIAS=${2}
+  local RECIPIENT=${3}
 
-  # If no specific alias was provided, match any alias key:
-  [[ -z ${MAIL_ALIAS} ]] && MAIL_ALIAS='\S\+'
+  # Validation error handling expects that the caller has defined a '__usage' method:
+  [[ -z ${MAIL_ALIAS} ]] && { __usage ; _exit_with_error 'No alias specified'     ; }
+  [[ -z ${RECIPIENT}  ]] && { __usage ; _exit_with_error 'No recipient specified' ; }
 
   local DATABASE_VIRTUAL='/tmp/docker-mailserver/postfix-virtual.cf'
-  _db_entry_remove "${DATABASE_VIRTUAL}" "${MAIL_ALIAS}" "${RECIPIENT}"
+  case "${ACTION}" in
+    # Associate RECIPIENT to MAIL_ALIAS:
+    ( 'update' )
+      _db_entry_add_or_append "${DATABASE_VIRTUAL}" "${MAIL_ALIAS}" "${RECIPIENT}"
+      ;;
+
+    # Removes RECIPIENT from MAIL_ALIAS - or all aliases when MAIL_ALIAS='_':
+    # NOTE: If a matched alias has no additional recipients, it is also removed.
+    ( 'delete' )
+      [[ ${MAIL_ALIAS} == '_' ]] && MAIL_ALIAS='\S\+'
+      _db_entry_remove "${DATABASE_VIRTUAL}" "${MAIL_ALIAS}" "${RECIPIENT}"
+      ;;
+
+    ( * ) # This should not happen if using convenience wrapper methods:
+      _exit_with_error "Unsupported Action: '${ACTION}'"
+      ;;
+
+  esac
 }
 
-# Returns a comma delimited list of aliases associated to a recipient (ideally the recipient is a mail account):
-function _alias_list_for_account
-{
-  local MAIL_ACCOUNT=${1}
-  local DATABASE_VIRTUAL='/tmp/docker-mailserver/postfix-virtual.cf'
-
-  function _account_has_an_existing_alias
-  {
-    grep -qi "${MAIL_ACCOUNT}" "${DATABASE_VIRTUAL}" 2>/dev/null
-  }
-
-  if [[ -f ${DATABASE_VIRTUAL} ]] && _account_has_an_existing_alias
-  then
-    grep "${MAIL_ACCOUNT}" "${DATABASE_VIRTUAL}" | awk '{print $1;}' | sed ':a;N;$!ba;s/\n/, /g'
-  fi
-}
-
-# Input validation:
-function _arg_expect_alias_and_recipient
-{
-  [[ -z ${MAIL_ALIAS} ]] && { __usage ; _exit_with_error 'No alias specified' ; }
-  [[ -z ${RECIPIENT} ]] && { __usage ; _exit_with_error 'No recipient specified' ; }
-}
+# Convenience wrappers:
+function _manage_virtual_aliases_update { _manage_virtual_aliases 'update' "${@}" ; } # addalias
+function _manage_virtual_aliases_delete { _manage_virtual_aliases 'delete' "${@}" ; } # delalias, delmailuser
