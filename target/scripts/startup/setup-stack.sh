f65078c8be1750759489e86be40e0d9fb80bde34
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2022-11-04 18:18:24 +0100

    adjust other tests
    The Postgrey tests needed adjustments as well.

--- target/scripts/startup/setup-stack.sh
+++ target/scripts/startup/setup-stack.sh
@@ -399,7 +399,7 @@ function _setup_postgrey
   _log 'debug' 'Configuring Postgrey'
 
   sed -i -E \
-    's|, reject_rbl_client zen.spamhaus.org$|, reject_rbl_client zen.spamhaus.org, check_policy_service inet:127.0.0.1:10023|' \
+    's|, reject_rbl_client zen.spamhaus.org=127.0.0.[2..11]$|, reject_rbl_client zen.spamhaus.org=127.0.0.[2..11], check_policy_service inet:127.0.0.1:10023|' \
     /etc/postfix/main.cf
 
   sed -i -e \
