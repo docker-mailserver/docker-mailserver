f3fe53d586d7f54c465e5f497609805d3951ee7f
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2022-11-05 16:28:51 +0100

    apply PR feedback

--- target/scripts/startup/setup-stack.sh
+++ target/scripts/startup/setup-stack.sh
@@ -398,8 +398,8 @@ function _setup_postgrey
 {
   _log 'debug' 'Configuring Postgrey'
 
-  sed -i -E \
-    's|, reject_rbl_client zen.spamhaus.org=127.0.0.[2..11]$|, reject_rbl_client zen.spamhaus.org=127.0.0.[2..11], check_policy_service inet:127.0.0.1:10023|' \
+  sedfile -i -E \
+    's|(smtpd_recipient_restrictions.*)|\1, check_policy_service inet:127.0.0.1:10023|' \
     /etc/postfix/main.cf
 
   sed -i -e \
