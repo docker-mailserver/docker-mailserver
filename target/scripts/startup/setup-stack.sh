e9e54e224dd6ab6b51d7b9167301eaceef38a9c7
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2023-02-09 13:48:44 +0100

    improve Amavis setup routine
    see https://github.com/docker-mailserver/docker-mailserver/pull/3046#issuecomment-1423718811

--- target/scripts/startup/setup-stack.sh
+++ target/scripts/startup/setup-stack.sh
@@ -73,14 +73,16 @@ function _setup_amavis
   if [[ ${ENABLE_AMAVIS} -eq 1 ]]
   then
     _log 'debug' 'Setting up Amavis'
+
     cat /etc/dms/postfix/master.d/postfix-amavis.cf >>/etc/postfix/master.cf
+    echo -e \
+      "\n# Additional option for filtering\ncontent_filter = smtp-amavis:[127.0.0.1]:10024" \
+      >>/etc/postfix/main.cf
+
     sed -i \
       "s|^#\$myhostname = \"mail.example.com\";|\$myhostname = \"${HOSTNAME}\";|" \
       /etc/amavis/conf.d/05-node_id
   else
-    _log 'debug' "Removing Amavis from Postfix's configuration"
-    sed -i 's|content_filter =.*|content_filter =|' /etc/postfix/main.cf
-
     _log 'debug' 'Disabling Amavis cron job'
     mv /etc/cron.d/amavisd-new /etc/cron.d/amavisd-new.disabled
     chmod 0 /etc/cron.d/amavisd-new.disabled
