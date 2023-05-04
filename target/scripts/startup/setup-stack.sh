8bdf4261be6cc2ee5a48f84b447d2934448bf9aa
LucidityCrash <luciditycrash@telinformix.com>
2023-05-04 09:20:32 +0000

    Updating to use setup.d folder

--- target/scripts/startup/setup-stack.sh
+++ target/scripts/startup/setup-stack.sh
@@ -57,45 +57,6 @@ function _early_supervisor_setup
   return 0
 }
 
-function _setup_getmail
-{
-  _log 'trace' 'Preparing Getmail configuration'
-
-  local GETMAILRC ID CONFIGS
-
-  GETMAILRC='/etc/getmailrc.d'
-  CONFIGS=false
-
-  if [[ ! -d ${GETMAILRC} ]]
-  then
-    mkdir "${GETMAILRC}"
-  fi
-
-  # Generate getmailrc configs, starting with the `/etc/getmailrc_general` base config,
-  # Add a unique `message_log` config, then append users own config to the end.
-  for FILE in /tmp/docker-mailserver/getmail-*.cf
-  do
-    if [[ -f ${FILE} ]]
-    then
-      CONFIGS=true
-      ID=$(cut -d '-' -f 3 <<< "${FILE}" | cut -d '.' -f 1)
-      local GETMAIL_CONFIG="${GETMAILRC}/getmailrc-${ID}"
-      cat /etc/getmailrc_general >"${GETMAIL_CONFIG}.tmp"
-      echo -e "message_log = /var/log/mail/getmail-${ID}.log\n" >>"${GETMAIL_CONFIG}.tmp"
-      cat "${GETMAIL_CONFIG}.tmp" "${FILE}" >"${GETMAIL_CONFIG}"
-      rm "${GETMAIL_CONFIG}.tmp"
-    fi
-  done
-  if [[ ${CONFIGS} == true ]]
-  then
-    cat >"/etc/cron.d/getmail" << EOF
-*/${GETMAIL_POLL} * * * * root /usr/local/bin/getmail-cron
-EOF
-    chmod -R 600 "${GETMAILRC}"
-  fi
-}
-
-
 function _setup_timezone
 {
   [[ -n ${TZ} ]] || return 0
