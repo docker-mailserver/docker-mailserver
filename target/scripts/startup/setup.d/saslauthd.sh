03fb1939ac06950ced5ea4b5a4b78f3c6500fb3a
polarathene <5098581+polarathene@users.noreply.github.com>
2023-09-01 13:54:32 +1200

    chore: Remove unset lines in generated `saslauthd.conf`

--- target/scripts/startup/setup.d/saslauthd.sh
+++ target/scripts/startup/setup.d/saslauthd.sh
@@ -11,25 +11,19 @@ function _setup_saslauthd() {
     _log 'trace' 'Creating /etc/saslauthd.conf'
 
     # Create a config based on ENV
-    # TODO: Remove keys with empty values via `sed`?
-    cat > /etc/saslauthd.conf << EOF
+    sed '/^.*: $/d'> /etc/saslauthd.conf << EOF
 ldap_servers: ${SASLAUTHD_LDAP_SERVER:=${LDAP_SERVER_HOST}}
-
 ldap_auth_method: ${SASLAUTHD_LDAP_AUTH_METHOD:=bind}
 ldap_bind_dn: ${SASLAUTHD_LDAP_BIND_DN:=${LDAP_BIND_DN}}
 ldap_bind_pw: ${SASLAUTHD_LDAP_PASSWORD:=${LDAP_BIND_PW}}
-
 ldap_search_base: ${SASLAUTHD_LDAP_SEARCH_BASE:=${LDAP_SEARCH_BASE}}
 ldap_filter: ${SASLAUTHD_LDAP_FILTER:=(&(uniqueIdentifier=%u)(mailEnabled=TRUE))}
-
 ldap_start_tls: ${SASLAUTHD_LDAP_START_TLS:=no}
 ldap_tls_check_peer: ${SASLAUTHD_LDAP_TLS_CHECK_PEER:=no}
-
 ldap_tls_cacert_file: ${SASLAUTHD_LDAP_TLS_CACERT_FILE}
 ldap_tls_cacert_dir: ${SASLAUTHD_LDAP_TLS_CACERT_DIR}
 ldap_password_attr: ${SASLAUTHD_LDAP_PASSWORD_ATTR}
 ldap_mech: ${SASLAUTHD_LDAP_MECH}
-
 ldap_referrals: yes
 log_level: 10
 EOF
