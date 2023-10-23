523c9f7923b969c464505bd36d9a7f3e527cff94
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2023-10-23 20:51:11 +0200

    provide better warning messages and comments

--- target/scripts/startup/setup.d/security/rspamd.sh
+++ target/scripts/startup/setup.d/security/rspamd.sh
@@ -82,6 +82,7 @@ function __rspamd__run_early_setup_and_checks() {
       ln -s "${RSPAMD_DMS_OVERRIDE_D}" "${RSPAMD_OVERRIDE_D}"
     else
       __rspamd__log 'warn' "Could not remove '${RSPAMD_OVERRIDE_D}' (not empty?; not a directory?; did you restart properly?) - not linking '${RSPAMD_DMS_OVERRIDE_D}'"
+      __rspamd__log 'warn' "Note that using '${RSPAMD_DMS_OVERRIDE_D}' and placing files manually in '${RSPAMD_OVERRIDE_D}' is not supported"
     fi
   fi
 
