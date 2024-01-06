5852ae56d36817a45c86834825e5926419904788
Casper <casperklein@users.noreply.github.com>
2024-01-06 03:31:07 +0100

    Update target/scripts/startup/setup.d/oauth2.sh

--- target/scripts/startup/setup.d/oauth2.sh
+++ target/scripts/startup/setup.d/oauth2.sh
@@ -4,7 +4,7 @@ function _setup_oauth2() {
   _log 'debug' 'Setting up OAUTH2'
 
   # Enable OAuth2 PassDB (Authentication):
-  sed -i -e '/\!include auth-oauth2\.conf\.ext/s/^#//' /etc/dovecot/conf.d/10-auth.conf
+  sedfile -i -e '/\!include auth-oauth2\.conf\.ext/s/^#//' /etc/dovecot/conf.d/10-auth.conf
   _replace_by_env_in_file 'OAUTH2_' '/etc/dovecot/dovecot-oauth2.conf.ext'
 
   return 0
