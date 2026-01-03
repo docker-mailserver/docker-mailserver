b351fb9322951cdda57de3daa79e5f34eb81032b
Matthieu Pignolet <matthieu@puffer.fish>
2026-01-03 15:42:21 +0100

    feat: fix linting issues

--- target/scripts/startup/setup.d/kerberos.sh
+++ target/scripts/startup/setup.d/kerberos.sh
@@ -5,12 +5,12 @@ function _setup_kerberos() {
 
   # Enable Kerberos PassDB (Authentication):
   # required fields:
-  # 
+  #
   # auth_gssapi_hostname = mail01.example.com
   # auth_krb5_keytab = /etc/dovecot/krb5.keytab
   # auth_realms = example.com
   # auth_default_realm = example.com
-  
+
   sedfile -i -e '/\!include auth-kerberos\.inc/s/^#//' /etc/dovecot/conf.d/10-auth.conf
   _replace_by_env_in_file 'KERBEROS_' '/etc/dovecot/conf.d/auth-kerberos.inc'
 
