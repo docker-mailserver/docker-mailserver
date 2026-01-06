e7854c05faa8796133d2468af8fcf7225ae5a92a
Cristian Spiridon <scristian71@gmail.com>
2026-01-06 21:41:04 +0000

    change restart with SIGUP fro rspamd

--- target/scripts/check-for-changes.sh
+++ target/scripts/check-for-changes.sh
@@ -206,8 +206,8 @@ function _rspamd_changes() {
       _log 'trace' 'Rspamd - DKIM files updated'
     fi
 
-    _log 'debug' 'Rspamd configuration has changed - restarting service'
-    supervisorctl restart rspamd
+    _log 'debug' 'Reloading Rspamd configuration'
+    pkill --signal SIGHUP $(pgrep --oldest rspamd)
   fi
 }
 
