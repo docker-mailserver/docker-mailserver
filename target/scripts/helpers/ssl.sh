b09b60345a7a4f9eae6531832739143036f5e877
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-02-15 14:17:15 +0100

    Apply suggestions from code review
    Co-authored-by: Brennan Kinney <5098581+polarathene@users.noreply.github.com>

--- target/scripts/helpers/ssl.sh
+++ target/scripts/helpers/ssl.sh
@@ -314,13 +314,9 @@ function _setup_ssl() {
         [[ ! -f ${SS_CERT} ]]    && MISSING_FILES+=("${SS_CERT}")
         [[ ! -f ${SS_CA_CERT} ]] && MISSING_FILES+=("${SS_CA_CERT}")
 
-        local ERROR_MESSAGE="One or more required files are missing. Please ensure all of the following files exist:"$'\n'
-        ERROR_MESSAGE+="  - ${SS_KEY}"$'\n'
-        ERROR_MESSAGE+="  - ${SS_CERT}"$'\n'
-        ERROR_MESSAGE+="  - ${SS_CA_CERT}"$'\n'
-        ERROR_MESSAGE+="Missing: ${MISSING_FILES[*]}"
-
-        _dms_panic__general "${ERROR_MESSAGE}" "${SCOPE_SSL_TYPE}"
+        # Concatenate each element and delimit with ` + `:
+        local ERROR_CONTEXT=$(printf "'%s' + " "${MISSING_FILES[@]}" | sed 's/ + $//')
+        _dms_panic__no_file "${ERROR_CONTEXT}" "${SCOPE_SSL_TYPE}"
       fi
       ;;
 
