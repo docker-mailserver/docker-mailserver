7a0cdc2f15c034c1bcb9eb27cfa2276e21f5dd6d
Brainkiller <brainkiller@boven.xyz>
2026-03-04 22:18:47 +0100

    Initial work

--- target/getmail/getmail-service.sh
+++ target/getmail/getmail-service.sh
@@ -3,6 +3,58 @@
 # shellcheck source=../scripts/helpers/log.sh
 source /usr/local/bin/helpers/log.sh
 
+function _main() {
+  if [[ ${GETMAIL_PARALLEL} -eq 1 ]]; then
+    _log 'debug' 'Getmail parallel is enabled, required 1 argument to specify the getmailrc file will be processed in a seperate service'
+    _require_n_parameters_or_print_usage 1 "${@}"
+  else
+    _log 'debug' 'Getmail parallel is disabled, running getmail in a loop'
+    _require_n_parameters_or_print_usage 0 "${@}"
+  fi
+
+  local RC_FILE="${1}"
+  local SERVICE="${SERVICE_NAME:-getmail}"
+
+  _validate_parameters
+  _add_relayhost
+}
+
+function __usage() {
+  printf '%s' "${PURPLE}getmail-service${RED}(${YELLOW}8${RED})
+
+${ORANGE}USAGE${RESET}
+    ./getmail-service.sh [<RC_FILE>]
+
+${ORANGE}OPTIONS${RESET}
+    ${BLUE}Generic service to launch getmail${RESET}
+        help       Print the usage information.
+
+${ORANGE}DESCRIPTION${RESET}
+    Run getmail in a loop, processing all configuration files in /etc/getmailrc.d/ periodically.
+    The period is defined by GETMAIL_POLL environment variable (in minutes).
+
+    If GETMAIL_PARALLEL is set, each configuration file is processed in a seperate service.
+    The variable GETMAIL_IDLE can be set to either a list of getmailrc files (e.g. 'getmail-1.rc,getmail-2.rc')
+    or to 'auto' to enable IMAP IDLE for all IMAP getmailrc files,
+
+${ORANGE}EXAMPLES${RESET}
+    ${LWHITE}./getmail-service.sh /etc/getmailrc.d/getmail-1.rc${RESET}
+        Process the getmail configuration file '/etc/getmailrc.d/getmail-1.rc'.
+
+    ${LWHITE}./getmail-service.sh${RESET}
+        Process all getmail configuration files in /etc/getmailrc.d/.
+
+${ORANGE}EXIT STATUS${RESET}
+    Exit status is 0 if command was successful. If wrong arguments are provided
+    or arguments contain errors, the script will exit early with exit status 1.
+
+"
+}
+
+function _validate_parameters() {
+  [[ -z ${RC_FILE} ]] && { __usage ; _exit_with_error 'No RC file specified'     ; }
+}
+
 # Directory, where "oldmail" files are stored.
 # getmail stores its state - its "memory" of what it has seen in your POP/IMAP account - in the oldmail files.
 GETMAIL_DIR=/var/lib/getmail
@@ -12,12 +64,12 @@ GETMAIL_DIR=/var/lib/getmail
 trap 'pkill --parent ${$}' EXIT
 
 function _syslog_error() {
-  logger --priority mail.err --tag getmail "${1}"
+  logger --priority mail.err --tag "${SERVICE}" "${1}"
 }
 
 function _stop_service() {
   _syslog_error "Stopping service"
-  exec supervisorctl stop getmail
+  exec supervisorctl stop "${SERVICE}"
 }
 
 # Verify the correct value for GETMAIL_POLL. Valid are any numbers greater than 0.
@@ -30,18 +82,43 @@ fi
 # If the nullglob option is set, and no matches are found, the word is removed.
 shopt -s nullglob
 
-# Run each getmailrc periodically.
-while :; do
-  for RC_FILE in /etc/getmailrc.d/*; do
-    _log 'debug' "Processing ${RC_FILE}"
-    getmail --getmaildir "${GETMAIL_DIR}" --rcfile "${RC_FILE}"
-  done
+if [[ -z "${RC_FILE}" ]]; then
+  # Run each getmailrc periodically. This is the default behavior when GETMAIL_PARALLEL is disabled.
+  while :; do
+    for RC_FILE_LOOP in /etc/getmailrc.d/*; do
+      _log 'debug' "Processing ${RC_FILE_LOOP}"
+      getmail --getmaildir "${GETMAIL_DIR}" --rcfile "${RC_FILE_LOOP}"
+    done
 
-  # Stop service if no configuration is found.
-  if [[ -z ${RC_FILE} ]]; then
-    _syslog_error 'No configuration found'
+    # Stop service if no configuration is found.
+    if [[ -z ${RC_FILE} ]]; then
+      _syslog_error 'No configuration found'
+      _stop_service
+    fi
+
+    sleep "${GETMAIL_POLL}m"
+  done
+elif [[ -n "${RC_FILE}" ]]; then
+  # Run the specified getmailrc file. This is the default behavior when GETMAIL_PARALLEL is enabled.
+  if [[ ! -f "${RC_FILE}" ]]; then
+    _syslog_error "Specified RC file does not exist: ${RC_FILE}"
     _stop_service
   fi
 
-  sleep "${GETMAIL_POLL}m"
-done
+  if grep -q 'IMAP' "${RC_FILE}" && [[ ${GETMAIL_IDLE} == "auto" || ${GETMAIL_IDLE} == *$(basename "${RC_FILE}")* ]]; then
+    _log 'debug' "Enabling IMAP IDLE for ${RC_FILE}"
+    getmail --getmaildir "${GETMAIL_DIR}" --rcfile "${RC_FILE}" --idle
+  else
+    _log 'debug' "IMAP IDLE not enabled for ${RC_FILE}"
+  fi
+
+  while :; do
+    _log 'debug' "Processing ${RC_FILE}"
+    getmail --getmaildir "${GETMAIL_DIR}" --rcfile "${RC_FILE}"
+
+    sleep "${GETMAIL_POLL}m"
+  done
+else
+  _syslog_error 'Invalid state: both RC_FILE and GETMAIL_PARALLEL are set'
+  _stop_service
+fi
--- test/tests/parallel/set1/getmail.bats
+++ test/tests/parallel/set1/getmail.bats
@@ -2,16 +2,41 @@ load "${REPOSITORY_ROOT}/test/helper/setup"
 load "${REPOSITORY_ROOT}/test/helper/common"
 
 BATS_TEST_NAME_PREFIX='[Getmail] '
-CONTAINER_NAME='dms-test_getmail'
+CONTAINER1_NAME='dms-test_getmail'
+CONTAINER2_NAME='dms-test_getmail_parallel'
+CONTAINER3_NAME='dms-test_getmail_parallel_specific'
 
 function setup_file() {
+  export CONTAINER_NAME
 
+  CONTAINER_NAME=${CONTAINER1_NAME}
   local CUSTOM_SETUP_ARGUMENTS=(--env 'ENABLE_GETMAIL=1')
+  _init_with_defaults
+  _common_container_setup 'CUSTOM_SETUP_ARGUMENTS'
+
+  CONTAINER_NAME=${CONTAINER2_NAME}
+  local CUSTOM_SETUP_ARGUMENTS=(
+    --env ENABLE_GETMAIL=1
+    --env GETMAIL_PARALLEL=1
+    --env LOG_LEVEL=debug
+  )
+  _init_with_defaults
+  _common_container_setup 'CUSTOM_SETUP_ARGUMENTS'
+
+  CONTAINER_NAME=${CONTAINER3_NAME}
+  local CUSTOM_SETUP_ARGUMENTS=(
+    --env ENABLE_GETMAIL=1
+    --env GETMAIL_PARALLEL=1
+    --env GETMAIL_IDLE='user3.cf,user4.cf'
+    --env LOG_LEVEL=debug
+  )
   _init_with_defaults
   _common_container_setup 'CUSTOM_SETUP_ARGUMENTS'
 }
 
-function teardown_file() { _default_teardown ; }
+function teardown_file() {
+    docker rm -f "${CONTAINER1_NAME}" "${CONTAINER2_NAME}" "${CONTAINER3_NAME}"
+}
 
 #? The file used in the following tests is placed in test/config/getmail/user3.cf
 
@@ -77,4 +102,53 @@ function teardown_file() { _default_teardown ; }
   assert_line '    to_oldmail_on_each_mail : False'
   assert_line '    use_netrc : False'
   assert_line '    verbose : 0'
+}
+
+@test "(ENV GETMAIL_PARALLEL=1, GETMAIL_IDLE=auto) should create seperate services and start idle on all IMAP configs" {
+  export CONTAINER_NAME=${CONTAINER2_NAME}
+
+  _wait_for_service getmail-1
+  _wait_for_service getmail-2
+  _wait_for_service getmail-3
+  _wait_for_service getmail-4
+
+  _service_log_should_contain_string "mail" "getmail-1"
+  _service_log_should_contain_string "mail" "getmail-2"
+  _service_log_should_contain_string "mail" "getmail-3"
+  _service_log_should_contain_string "mail" "getmail-4"
+
+  _service_log_should_contain_string "mail" "Enabling IMAP IDLE for user3.cf"
+  _service_log_should_contain_string "mail" "Enabling IMAP IDLE for user4.cf"
+  _service_log_should_contain_string "mail" "Enabling IMAP IDLE for user5.cf"
+  _service_log_should_not_contain_string "mail" "Enabling IMAP IDLE for user6.cf"
+
+  _service_log_should_not_contain_string "mail" "Processing user3.cf"
+  _service_log_should_not_contain_string "mail" "Processing user4.cf"
+  _service_log_should_not_contain_string "mail" "Processing user5.cf"
+  _service_log_should_contain_string "mail" "Processing user6.cf"
+}
+
+@test "(ENV GETMAIL_PARALLEL=1, GETMAIL_IDLE=user3.cf,user4.cf) should create seperate services and only start idle on 2 configs" {
+  export CONTAINER_NAME=${CONTAINER3_NAME}
+
+  _wait_for_service getmail-1
+  _wait_for_service getmail-2
+  _wait_for_service getmail-3
+  _wait_for_service getmail-4
+
+  _service_log_should_contain_string "mail" "getmail-1"
+  _service_log_should_contain_string "mail" "getmail-2"
+  _service_log_should_contain_string "mail" "getmail-3"
+  _service_log_should_contain_string "mail" "getmail-4"
+
+  _service_log_should_contain_string "mail" "Enabling IMAP IDLE for user3.cf"
+  _service_log_should_contain_string "mail" "Enabling IMAP IDLE for user4.cf"
+  _service_log_should_not_contain_string "mail" "Enabling IMAP IDLE for user5.cf"
+  _service_log_should_not_contain_string "mail" "Enabling IMAP IDLE for user6.cf"
+
+  _service_log_should_not_contain_string "mail" "Processing user3.cf"
+  _service_log_should_not_contain_string "mail" "Processing user4.cf"
+  _service_log_should_contain_string "mail" "Processing user5.cf"
+  _service_log_should_contain_string "mail" "Processing user6.cf"
+
 }
